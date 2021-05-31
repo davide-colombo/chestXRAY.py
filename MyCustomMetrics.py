Bug fixed in precision, recall and f1 score metrics
tf.cast to float32 required

MyCustomMetrics.py
@@ -14,15 +14,21 @@ class MyCustomMetrics:
 
     def __categorical_macro_recall(self, y_true, y_pred):
         cm = self.__categorical_confusion_mat(y_true, y_pred)
-        true_pos = tf.linalg.diag_part(cm)
+        true_pos  = tf.linalg.diag_part(cm)
         false_neg = tf.subtract(tf.reduce_sum(cm, axis=1), true_pos)
-        return tf.divide(true_pos, tf.add(true_pos, false_neg))
+        num = tf.cast(true_pos, tf.float32)
+        den = tf.cast(tf.add(true_pos, false_neg), tf.float32)
+        den = tf.add(den, 1e-7)
+        return tf.divide(num, den)
 
     def __categorical_macro_precision(self, y_true, y_pred):
         cm = self.__categorical_confusion_mat(y_true, y_pred)
         true_pos = tf.linalg.diag_part(cm)
         false_pos = tf.subtract(tf.reduce_sum(cm, axis=0), true_pos)
-        return tf.divide(true_pos, tf.add(true_pos, false_pos))
+        num = tf.cast(true_pos, tf.float32)
+        den = tf.cast(tf.add(true_pos, false_pos), tf.float32)
+        den = tf.add(den, 1e-7)
+        return tf.divide(num, den)
 
     def __get_class_weight(self, y_true, y_pred):
         cm = self.__categorical_confusion_mat(y_true, y_pred)
@@ -52,26 +58,25 @@ class MyCustomMetrics:
         return true_pos / (false_pos + true_pos)
 
     def macro_weighted_precision(self, y_true, y_pred):
-        class_weight = self.__get_class_weight(y_true, y_pred)
-        class_precision = self.__categorical_macro_precision(y_true, y_pred)
+        class_weight    = tf.cast(self.__get_class_weight(y_true, y_pred), tf.float32)
+        class_precision = tf.cast(self.__categorical_macro_precision(y_true, y_pred), tf.float32)
         return tf.reduce_sum(tf.multiply(class_precision, class_weight))
 
     def macro_weighted_recall(self, y_true, y_pred):
-        class_recall = self.__categorical_macro_recall(y_true, y_pred)
-        class_weight = self.__get_class_weight(y_true, y_pred)
+        class_recall = tf.cast(self.__categorical_macro_recall(y_true, y_pred), tf.float32)
+        class_weight = tf.cast(self.__get_class_weight(y_true, y_pred), tf.float32)
         return tf.reduce_sum(tf.multiply(class_recall, class_weight))
 
     def macro_weighted_f1score(self, y_true, y_pred):
-        class_weight = self.__get_class_weight(y_true, y_pred)
-        p = self.__categorical_macro_precision(y_true, y_pred)
-        r = self.__categorical_macro_recall(y_true, y_pred)
+        class_weight = tf.cast(self.__get_class_weight(y_true, y_pred), tf.float32)
+        p            = tf.cast(self.__categorical_macro_precision(y_true, y_pred), tf.float32)
+        r            = tf.cast(self.__categorical_macro_recall(y_true, y_pred), tf.float32)
         num = tf.multiply(p, r)
-        num = tf.multiply(2, num)
+        num = tf.multiply(num, 2.0)
         den = tf.add(p, r)
         den = tf.add(den, 1e-7)
         f1score = tf.divide(num, den)
         return tf.reduce_sum(tf.multiply(f1score, class_weight))
-
 
     def categorical_recall(self, y_true, y_pred):
         true_pos  = self.categorical_true_positives(y_true, y_pred)
@@ -103,34 +108,34 @@ class MyCustomMetrics:
 
 # THIS IS A TEST
 
-# from sklearn.metrics import f1_score
-# from sklearn.metrics import balanced_accuracy_score
-# from sklearn.metrics import accuracy_score
-#
-# my_custom_metrics = MyCustomMetrics()
-#
-# # CHECK
-# my_custom_metrics.categorical_weighted_accuracy(y_true = [[0, 1, 0],       [0, 1, 0],       [0, 0, 1],       [1, 0, 0], [0, 0, 1]],
-#                                          y_pred = [[0.1, 0.8, 0.1], [0.2, 0.7, 0.1], [0.4, 0.3, 0.3], [0, 1, 0], [0, 0, 1]])
-#
-# f1_score(y_true = [1, 1, 2, 0, 2],
-#          y_pred = [1, 1, 0, 1, 2],
-#          average = 'weighted')
-#
-# balanced_accuracy_score(y_true = [1, 1, 2, 0, 2],
-#                         y_pred = [1, 1, 0, 1, 2])
-#
-# accuracy_score(y_true = [1, 1, 2, 0, 2],
-#                y_pred = [1, 1, 0, 1, 2],
-#                normalize=True)
-#
-# from sklearn.metrics import precision_score
-# from sklearn.metrics import recall_score
-#
-# precision_score(y_true = [1, 1, 2, 0, 2],
-#                 y_pred = [1, 1, 0, 1, 2],
-#                 average = 'weighted')
-#
-# recall_score(y_true = [1, 1, 2, 0, 2],
-#              y_pred = [1, 1, 0, 1, 2],
-#              average = 'weighted')
+from sklearn.metrics import f1_score
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import accuracy_score
+
+my_custom_metrics = MyCustomMetrics()
+
+# CHECK
+my_custom_metrics.macro_weighted_f1score(y_true = [[0, 1, 0],       [0, 1, 0],       [0, 0, 1],       [1, 0, 0], [0, 0, 1]],
+                                         y_pred = [[0.1, 0.8, 0.1], [0.2, 0.7, 0.1], [0.4, 0.3, 0.3], [0, 1, 0], [0, 0, 1]])
+
+f1_score(y_true = [1, 1, 2, 0, 2],
+         y_pred = [1, 1, 0, 1, 2],
+         average = 'weighted')
+
+balanced_accuracy_score(y_true = [1, 1, 2, 0, 2],
+                        y_pred = [1, 1, 0, 1, 2])
+
+accuracy_score(y_true = [1, 1, 2, 0, 2],
+               y_pred = [1, 1, 0, 1, 2],
+               normalize=True)
+
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+precision_score(y_true = [1, 1, 2, 0, 2],
+                y_pred = [1, 1, 0, 1, 2],
+                average = 'weighted')
+
+recall_score(y_true = [1, 1, 2, 0, 2],
+             y_pred = [1, 1, 0, 1, 2],
+             average = 'weighted')